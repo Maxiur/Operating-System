1135e6ec91cec62359aec07ee9d2a1543d9e262d
Maxiur <mateusz222244@op.pl>
2025-04-02 10:21:39 +0200

    Cwiczenie3

--- Cwiczenie3/3c_leader.c
+++ Cwiczenie3/3c_leader.c
@@ -83,7 +83,7 @@ int main(int argc, char *argv[]) {
 
         if(WIFSIGNALED(status)) {
           int sig = WTERMSIG(status);
-          printf("[Lider]    --> Zabił go sygnał %d (%s)\n", sig, strsignal(sig));
+          printf("[Lider]    --> Zabił go sygnał %d (%s) (kod: %d)\n", sig, strsignal(sig),WEXITSTATUS(status));
         }
         else if(WIFEXITED(status))
           printf("[Lider]    --> Zakończony normalnie (kod: %d)\n", status);
