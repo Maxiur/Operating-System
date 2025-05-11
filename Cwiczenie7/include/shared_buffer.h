35fc5c073d63fd56ff2193cf869a98acfc97e5c1
Maxiur <mateusz222244@op.pl>
2025-05-11 21:22:14 +0200

    programiki

--- Cwiczenie7/include/shared_buffer.h
+++ Cwiczenie7/include/shared_buffer.h
@@ -5,14 +5,21 @@
 #define NBUF 5
 
 typedef struct {
-    int buffer[NBUF][NELE];
+    char element[NELE];
+} Towar; // jednoska towaru (danych)
+
+typedef struct {
+    Towar bufor[NBUF];
     int wstaw;
     int wyjmij;
 } SegmentPD;
+
+#define ILE_TOWAROW 10
 
 #define SEM_WRITE_TO_SHM "/SEM_WRITE_TO_SHM"
 #define SEM_READ_FROM_SHM "/SEM_READ_FROM_SHM"
 #define SHM_NAME "/sharred_buffer"
 #define SEM_NAME "my_semaphore"
+#define SHM_SIZE sizeof(SegmentPD)
 
 #endif // SHARED_BUFFER_H
